6e5a4c85453e89b4210bdb98b14db573691156da
AREUM\lee99 <lee990103@naver.com>
2022-10-03 23:54:22 +0900

    JPA, AOP 개념 및 흐름 파악하기

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,12 +4,12 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 
 public class MemberService {
+    
+    private MemberRepository memberRepository;
 
-    private final MemberRepository memberRepository;
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -17,28 +17,38 @@ public class MemberService {
     /**
      * 회원가입
      */
-    public Long join(Member member){
-        //같은 이름이 있는 중복 회원x
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+    public Long join(Member member) {
+        
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
 
     /**
      * 전체 회원 조회
      */
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
-    }
-
-    public Optional<Member> findOne(Long memberId){
-        return memberRepository.findById(memberId);
+    public List<Member> findMembers() {
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 }
--- hello-spring/hello-spring/src/main/java/hello/hellospring/springConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/springConfig.java
@@ -1,21 +1,28 @@
 package hello.hellospring;
-
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
-public class springConfig {
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
     }
 }
